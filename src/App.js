131165fd7a4191d1f8a70930b18985d3072c6bce
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-26 10:14:40 -0700

    added branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useRef } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
 import { SearchContext } from './context/SearchContext'
+
 
 function App() {
     let [message, setMessage] = useState('Search for Music!')
